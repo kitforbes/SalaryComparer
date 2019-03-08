1f69a5f964d6f3a524d42ad5f87455af0a31150a
Chris Forbes <git@chrisforbes.dev>
2019-03-08 22:33:25 +0000

    Add initial pension calculations

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SalaryComparer
 
 Simple tool to compare two UK salaries.
+
+## Usage
+
+```powershell
+# dotnet run --project .\SalaryComparer <salary> <pension-contribution-rate> <employer-pension-contribution-rate> <salary> <pension-contribution-rate> <employer-pension-contribution-rate>
+dotnet run --project .\SalaryComparer 20000 0.1 0.05 20000 0.2 0.05
+```
--- SalaryComparer/Program.cs
+++ SalaryComparer/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace SalaryComparer
+﻿using System;
+
+namespace SalaryComparer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var currentSalary = double.Parse(args[0]);
+            var currentPensionContributionRate = double.Parse(args[1]);
+            var currentPensionContribution = currentSalary * currentPensionContributionRate;
+            var currentEmployerPensionContributionRate = double.Parse(args[2]);
+            var currentEmployerPensionContribution = currentSalary * currentEmployerPensionContributionRate;
+            var currentTotalPensionContribution = currentPensionContribution + currentEmployerPensionContribution;
+
+            var newSalary = double.Parse(args[3]);
+            var newPensionContributionRate = double.Parse(args[4]);
+            var newPensionContribution = newSalary * newPensionContributionRate;
+            var newEmployerPensionContributionRate = double.Parse(args[5]);
+            var newEmployerPensionContribution = newSalary * newEmployerPensionContributionRate;
+            var newTotalPensionContribution = newPensionContribution + newEmployerPensionContribution;
+
+            var output = $@"
+                              |   Current   |  Proposed   | Difference  |
+------------------------------|-------------|-------------|-------------|
+Salary                        | {currentSalary,11:C} | {newSalary,11:C} | {newSalary - currentSalary,11:C} |
+Pension Contribution          | {currentPensionContribution,11:C} | {newPensionContribution,11:C} | {newPensionContribution - currentPensionContribution,11:C} |
+Employer Pension Contribution | {currentEmployerPensionContribution,11:C} | {newEmployerPensionContribution,11:C} | {newEmployerPensionContribution - currentEmployerPensionContribution,11:C} |
+Total Pension Contribution    | {currentTotalPensionContribution,11:C} | {newTotalPensionContribution,11:C} | {newTotalPensionContribution - currentTotalPensionContribution,11:C} |
+";
+
+            Console.WriteLine(output);
         }
     }
 }
