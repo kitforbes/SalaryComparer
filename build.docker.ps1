8966182289f6ba6bba1973114587f98783d4d1e1
Chris Forbes <git@chrisforbes.dev>
2019-03-09 15:14:06 +0000

    Add Docker build script files

---
+++ build.docker.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = "Stop"
+
+Set-Location -Path $PSScriptRoot
+
+docker build -t salary-comparer .
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+docker run --rm salary-comparer $args
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+exit 0
