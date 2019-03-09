6436835168596be5d560a0e14a7e09e4339270f0
Chris Forbes <git@chrisforbes.dev>
2019-03-09 12:23:55 +0000

    Add functions for writing to the console

--- SalaryComparer/Program.cs
+++ SalaryComparer/Program.cs
@@ -9,16 +9,30 @@ namespace SalaryComparer
             var current = new Salary(double.Parse(args[0]), double.Parse(args[1]), double.Parse(args[2]));
             var proposed = new Salary(double.Parse(args[3]), double.Parse(args[4]), double.Parse(args[5]));
 
-            var output = $@"
+            WriteToConsole($@"
                               |   Current   |  Proposed   | Difference  |
-------------------------------|-------------|-------------|-------------|
-Salary                        | {current.Amount,11:C} | {proposed.Amount,11:C} | {proposed.Amount - current.Amount,11:C} |
-Pension Contribution          | {current.EmployeeContribution.Amount,11:C} | {proposed.EmployeeContribution.Amount,11:C} | {proposed.EmployeeContribution.Amount - current.EmployeeContribution.Amount,11:C} |
-Employer Pension Contribution | {current.EmployerContribution.Amount,11:C} | {proposed.EmployerContribution.Amount,11:C} | {proposed.EmployerContribution.Amount - current.EmployerContribution.Amount,11:C} |
-Total Pension Contribution    | {current.TotalPensionContribution,11:C} | {proposed.TotalPensionContribution,11:C} | {proposed.TotalPensionContribution - current.TotalPensionContribution,11:C} |
-";
+------------------------------|-------------|-------------|-------------|");
+            WriteCurrencyLine("Salary                       ", new[] { current.Amount, proposed.Amount, proposed.Amount - current.Amount });
+            WriteCurrencyLine("Pension Contribution         ", new[] {
+current.EmployeeContribution.Amount, proposed.EmployeeContribution.Amount, proposed.EmployeeContribution.Amount - current.EmployeeContribution.Amount });
+            WriteCurrencyLine("Employer Pension Contribution", new[] { current.EmployerContribution.Amount, proposed.EmployerContribution.Amount, proposed.EmployerContribution.Amount - current.EmployerContribution.Amount });
+            WriteCurrencyLine("Total Pension Contribution   ", new[] { current.TotalPensionContribution, proposed.TotalPensionContribution, proposed.TotalPensionContribution - current.TotalPensionContribution });
+        }
 
-            Console.WriteLine(output);
+        private static void WriteCurrencyLine(string lineTitle, double[] items, string separator = " | ")
+        {
+            var output = $"{lineTitle}{separator}";
+            foreach (var item in items)
+            {
+                output += $"{item,11:C}{separator}";
+            }
+
+            WriteToConsole(output);
+        }
+
+        private static void WriteToConsole(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
